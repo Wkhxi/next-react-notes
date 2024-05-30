0f85c2936c2a869ce196a4121cddecb06b1c61c5
wkh <1531685233@qq.com>
2024-05-30 15:53:20 +0800

    feat: search bar-1

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -12,6 +12,10 @@ export default async function NoteList() {
   }
 
   return (
+    // SidebarNoteListFilter 包了一层 这么写是为了 让 SidebarNoteItem 还是服务端组件
+    // SidebarNoteItem以props的形式 传递进 SidebarNoteListFilter
+    // 服务端组件可以引入客户端组件，客户端组件不能引入服务端组件（因为一引入这个服务端组件就变为客户端组件了）
+    // 所以将 服务端组件以props形式传入客户端组件，来达到 客户端组件引入服务端组件 的目的
     <SidebarNoteListFilter>
       {Object.entries(notes).map(([noteId, note]) => {
         return (
